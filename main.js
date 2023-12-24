53c7cd300826cdf1bc909b0c13879b0c53539ac0
Ta-Run <tarnsharma11091999@gmail.com>
2023-12-25 00:01:01 +0530

    make responsive csss and add tailwind

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
- var typed= new typed(".text",{
+ var typed= new Typed(".text",{
     Strings:["Frontend Developer","Backend Developer"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
     loop: true
 });
+
+
