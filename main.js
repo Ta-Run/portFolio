8f26bdfd7b857b90fd9a0d9968bffc1a07f8e223
Ta-Run <tarnsharma11091999@gmail.com>
2024-01-30 23:38:01 +0530

    add progress bar

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
- var typed= new Typed(".text",{
-    Strings:["Frontend Developer","Backend Developer"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
+var typed = new Typed(".text", {
+    Strings: ["Frontend Developer", "Backend Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
     loop: true
 });
 
-function onToggleMenu(name){
-    console.log('event',name)
+function onToggleMenu(name) {
+    console.log('event', name)
 }
 
 
+let circularProgress = document.querySelector('.circular-progress');
+progressValue = document.querySelector('.progress-value');
+
+let progressStartValue = 0;
+progressEndValue = 90;
+speed = 100;
+
+let progress = setInterval(() => {
+    progressStartValue++;
+    progressValue.textContent = `${progressStartValue}%`
+    circularProgress.style.background = `conic-gradient(#9233ea ${progressStartValue * 3.6}deg,#FFFFFF,#FFFFFF)`
+    if (progressStartValue == progressEndValue) {
+        clearInterval(progress)
+    }
+
+}, speed);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'blue-900': '#3b82f6',
+        'gray-300': '#e5e7eb',
+        'pink-600': '#e65acb',
+        'red-800': '#991b1b',
+      },
+    },
   },
   plugins: [],
 }
