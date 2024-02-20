7bec4fa8f8aad451a43f77e3070b341c01ef9a2b
Ta-Run <tarnsharma11091999@gmail.com>
2024-02-20 13:20:12 +0530

    make progress bar dynmic

--- main.js
+++ main.js
@@ -10,19 +10,23 @@ function onToggleMenu(name) {
     console.log('event', name)
 }
 
+let circularProgressBars = document.querySelectorAll('.circular-progress');
+let progressValues = document.querySelectorAll('.progress-value');
 
-let circularProgress = document.querySelector('.circular-progress');
-progressValue = document.querySelector('.progress-value');
+let progressEndValue = 90;
+let speed = 100;
 
-let progressStartValue = 0;
-progressEndValue = 90;
-speed = 100;
+circularProgressBars.forEach((circularProgress, index) => {
+    let progressStartValue = 0; // Reset progress start value for each progress bar
+    let progress = setInterval(() => {
+        if (progressStartValue >= progressEndValue) {
+            clearInterval(progress);
+        } else {
+            progressStartValue++;
+            progressValues[index].textContent = `${progressStartValue}%`;
+            circularProgress.style.background = `conic-gradient(#9233ea ${progressStartValue * 3.6}deg,#FFFFFF,#FFFFFF)`;
+        }
+    }, speed);
+});
 
-let progress = setInterval(() => {
-    progressStartValue++;
-    progressValue.textContent = `${progressStartValue}%`
-    circularProgress.style.background = `conic-gradient(#9233ea ${progressStartValue * 3.6}deg,#FFFFFF,#FFFFFF)`
-    if (progressStartValue == progressEndValue) {
-        clearInterval(progress)
-    }
-}, speed);
+
